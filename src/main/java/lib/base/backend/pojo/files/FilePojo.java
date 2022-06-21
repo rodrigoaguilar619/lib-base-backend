a37c65b38506af53f985c636bd3fde11cdc82be1
Rodrigo Aguilar <rodrigoaguilar619@gmail.com>
2022-06-21 16:46:06 -0500

    Added file pojo

---
+++ src/main/java/lib/base/backend/pojo/files/FilePojo.java
@@ -0,0 +1,25 @@
+package lib.base.backend.pojo.files;
+
+public class FilePojo {
+
+	private String fileBase64;
+	
+	private String mimeType;
+
+	public String getFileBase64() {
+		return fileBase64;
+	}
+
+	public void setFileBase64(String fileBase64) {
+		this.fileBase64 = fileBase64;
+	}
+
+	public String getMimeType() {
+		return mimeType;
+	}
+
+	public void setMimeType(String mimeType) {
+		this.mimeType = mimeType;
+	}
+	
+}
