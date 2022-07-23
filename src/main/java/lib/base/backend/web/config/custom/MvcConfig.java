910edb3188929167d2fcc88752f791f7cb23cae9
Rodrigo Aguilar <rodrigoaguilar619@gmail.com>
2022-07-23 12:21:18 -0500

    Configuration swagger

--- src/main/java/lib/base/backend/web/config/custom/MvcConfig.java
+++ src/main/java/lib/base/backend/web/config/custom/MvcConfig.java
@@ -1,6 +1,7 @@
 package lib.base.backend.web.config.custom;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -12,5 +13,14 @@ public class MvcConfig implements WebMvcConfigurer {
 		registry.addViewController("/").setViewName("homepage");
 		registry.addViewController("/hello").setViewName("hello");
 		registry.addViewController("/login").setViewName("login");
+	}
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+	    registry.addResourceHandler("swagger-ui.html")
+	      .addResourceLocations("classpath:/META-INF/resources/");
+
+	    registry.addResourceHandler("/webjars/**")
+	      .addResourceLocations("classpath:/META-INF/resources/webjars/");
 	}
 }
