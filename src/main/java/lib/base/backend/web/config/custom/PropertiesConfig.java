5aeafe35d6601e83561d1b3e317d20d6b8c08aa6
rodrigoaguilar619 <rodrigoaguilar619@gmail.com>
2023-08-02 10:36:39 -0500

    Implementation of configuration for spring actuator

--- src/main/java/lib/base/backend/web/config/custom/PropertiesConfig.java
+++ src/main/java/lib/base/backend/web/config/custom/PropertiesConfig.java
@@ -2,9 +2,13 @@ package lib.base.backend.web.config.custom;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @Configuration
-@PropertySource("classpath:application.properties")
+@PropertySources({
+	@PropertySource("classpath:application_default.properties"),
+    @PropertySource("classpath:application.properties")
+})
 public class PropertiesConfig {
 
 }
