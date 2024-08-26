b7bb6cefe26eab0a74e1825bfab6feda43b260e9
rodrigoaguilar619 <rodrigoaguilar619@gmail.com>
2024-08-25 22:21:11 -0600

    Added jpa util method to get native table name defined on entity

--- src/main/java/lib/base/backend/utils/JpaUtil.java
+++ src/main/java/lib/base/backend/utils/JpaUtil.java
@@ -1,5 +1,6 @@
 package lib.base.backend.utils;
 
+import jakarta.persistence.Table;
 import jakarta.persistence.metamodel.StaticMetamodel;
 
 public class JpaUtil {
@@ -8,4 +9,14 @@ public class JpaUtil {
 		
 		return (clazz.getAnnotation(StaticMetamodel.class)).value().getSimpleName();
 	}
+	
+	public String getTableName(Class<?> entityClass) {
+        Table tableAnnotation = entityClass.getAnnotation(Table.class);
+
+        if (tableAnnotation != null && !tableAnnotation.name().isEmpty()) {
+            return tableAnnotation.name();
+        } else {
+            return entityClass.getSimpleName();
+        }
+    }
 }
