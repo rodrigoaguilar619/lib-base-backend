af129743106f9bf038d146ba21a60c65aa4a519d
rodrigoaguilar619 <rodrigoaguilar619@gmail.com>
2024-09-19 22:57:54 -0600

    Renamed swagger class and define API with JWT config

---
+++ src/main/java/lib/base/backend/modules/web/config/OpenApiConfig.java
@@ -0,0 +1,44 @@
+package lib.base.backend.modules.web.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class OpenApiConfig {
+	
+	@Value( value = "${springdoc.swagger.api.title}")
+	private String title;
+
+	@Value("${springdoc.swagger.api.version}")
+	private String version;
+    
+	@Bean
+	Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.withClassAnnotation(RestController.class)).paths(PathSelectors.any())
+				.build();
+	}
+
+    @Bean
+    OpenAPI customOpenAPI() {
+    	
+        OpenAPI openAPI =  new OpenAPI().info(new Info().title(title != null ? title : "").version(version != null ? version : ""))
+				.components(new Components().addSecuritySchemes("bearerAuth",
+						new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")))
+				.addSecurityItem(new SecurityRequirement().addList("bearerAuth"));
+        
+        return openAPI;
+	}
+}
