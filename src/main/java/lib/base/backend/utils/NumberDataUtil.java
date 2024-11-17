a9542f0ecf95f984eb97224f2ce5882b67b9db5b
rodrigoaguilar619 <rodrigoaguilar619@gmail.com>
2024-11-16 23:27:36 -0600

    Added method fo verify fractional part with BigDecimal value

--- src/main/java/lib/base/backend/utils/NumberDataUtil.java
+++ src/main/java/lib/base/backend/utils/NumberDataUtil.java
@@ -1,8 +1,14 @@
 package lib.base.backend.utils;
 
+import java.math.BigDecimal;
+
 public class NumberDataUtil {
 
 	public boolean hasFractionalPart(double value) {
         return value % 1 != 0;
     }
+
+	public boolean hasFractionalPart(BigDecimal value) {
+		return value.remainder(BigDecimal.ONE).compareTo(BigDecimal.ZERO) != 0;
+	}
 }
